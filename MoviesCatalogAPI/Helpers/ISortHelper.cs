cd0fbd17a70fd343317dfc0f8c3c3977d8489fa3
Roberto Fuentes <robertojeremy@gmail.com>
2023-04-05 12:28:01 -0600

    Adding Sorting functionality

---
+++ MoviesCatalogAPI/Helpers/ISortHelper.cs
@@ -0,0 +1,7 @@
+﻿namespace MoviesCatalogAPI.Helpers
+{
+    public interface ISortHelper<T>
+    {
+        IQueryable<T> ApplySort(IQueryable<T> entities, string orderByQueryString);
+    }
+}
--- MoviesCatalogAPI/Models/MovieParameters.cs
+++ MoviesCatalogAPI/Models/MovieParameters.cs
@@ -2,7 +2,10 @@
 {
     public class MovieParameters : QueryStringParameters
     {
-
+        public MovieParameters() 
+        {
+            OrderBy = "MovieReleaseyear";
+        }
         public string Category { get; set; } = string.Empty;
         public string Yearrelease { get; set; } = string.Empty;
         public string Searchby { get; set; } = string.Empty;
